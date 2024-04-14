59aa9f4793613b7239ed301f835f898e27a2d649
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-04-14 10:27:02 +0300

    complete dthe sending to server with fetch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+// SEND DATA TO SERVER
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",{
+        method : "POST",
+        headers : {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+
+        },
+        body:JSON.stringify({
+            name,
+            email
+
+        })
+
+    } )
+    // Handle the response from the server
+    .then(function ( response){
+        return response.json()
+    })
+    .then(function ( object){
+        document.body.innerHTML = object[ "id" ]
+    })
+    // Handle errors
+    .catch(function (error){
+        document.body.innerHTML = error.message
+    })
+}
